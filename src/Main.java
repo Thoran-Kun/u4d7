d324e1c698ec8fc10faece19fbda4bd9300bac93
Salvatore <pepesalvatore12@gmail.com>
2026-01-14 16:56:10 +0100

    add ese 2

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,15 @@ void main() {
     //creo la lista dei prodotti
     List<Product> productList = new ArrayList<>();
     productList.add(new Product(1L, "Il Nome della Rosa", "Book", 65.00));
-    productList.add(new Product(2L, "Il Codice Da Vinci", "Book", 70.00));
+    productList.add(new Product(2L, "Ciuccio", "Baby", 70.00));
     productList.add(new Product(3L, "Il Silmarillion", "Book", 80.00));
     productList.add(new Product(4L, "Harry Potter e la Pietra Filosofale", "Book", 75.00));
-    productList.add(new Product(5L, "Il Signore degli Anelli", "Book", 120.00));
+    productList.add(new Product(5L, "Il Signore dei passeggini", "Baby", 120.00));
     productList.add(new Product(6L, "La Ragazza con il Tatuaggio del Drago", "Book", 90.00));
     productList.add(new Product(7L, "Il Gioco delle Ombre", "Book", 85.00));
-    productList.add(new Product(8L, "La Storia Infinita", "Book", 100.00));
+    productList.add(new Product(8L, "La Storia Infinita", "Baby", 100.00));
     productList.add(new Product(9L, "Il Pendolo di Foucault", "Book", 110.00));
-    productList.add(new Product(10L, "La Città & La Città", "Book", 125.00));
+    productList.add(new Product(10L, "La Culla Perduta", "Baby", 125.00));
     //------------------------------------------------------------------------------------
     //creo la lista dei customer
     List<Customer> customerList = new ArrayList<>();
@@ -32,11 +32,16 @@ void main() {
     orderList.add(new Order(1L, "delivered", LocalDate.now(), LocalDate.now().plusDays(3), List.of(productList.get(2)), c1));
     //------------------------------------------------------------------------------------
 
+    //ESERCIZIO 1
     List<Product> booksMax100 = productList.stream()
             .filter(p -> p.getCategory().equals("Book"))
             .filter(p -> p.getPrice() >= 100)
             .toList();
-    System.out.println(booksMax100);
+    System.out.println("I libri che costano 100€ o di più sono: "booksMax100);
 
+    //ESERCIZIO 2
+    List<Product> babyPoducts = productList.stream()
+            .filter(p -> p.getCategory().equals("Baby")).toList();
+    System.out.println("I prodotti dei bambini sono: "babyPoducts);
 
 }
